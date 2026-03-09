0cf3246e97a489cfe3c887ef069f617d67da5789
hc-45 <223769218+hc-45@users.noreply.github.com>
2026-03-09 13:26:20 +0000

    FEAT: maplesim arena periodic

--- src/main/java/com/stuypulse/robot/Robot.java
+++ src/main/java/com/stuypulse/robot/Robot.java
@@ -20,6 +20,8 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
+import org.ironmaple.simulation.SimulatedArena;
+
 import com.ctre.phoenix6.SignalLogger;
 
 public class Robot extends TimedRobot {
@@ -67,6 +69,11 @@ public class Robot extends TimedRobot {
     //     SmartDashboard.putNumber("FMSUtil/Time Left In Shift", fmsUtil.getTimeLeftInShift());
     //     SmartDashboard.putBoolean("FMSUtil/Is Active Shift?", fmsUtil.isActiveShift());
     //     SmartDashboard.putString("FMSUtil/Field State", fmsUtil.getCurrentFieldState().toString());
+    }
+
+    @Override
+    public void simulationPeriod() {
+        SimulatedArena.getInstance().simulationPeriodic();
     }
 
     /*********************/
